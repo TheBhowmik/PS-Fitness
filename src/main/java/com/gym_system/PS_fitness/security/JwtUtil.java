fd726830470ea7a106d24627de81073146ee15f6
TheBhowmik <bhowmikniladri414@gmail.com>
2026-08-26 20:38:50 +0530

    refactor: Remove unused import for SignatureAlgorithm in JwtUtil

--- src/main/java/com/gym_system/PS_fitness/security/JwtUtil.java
+++ src/main/java/com/gym_system/PS_fitness/security/JwtUtil.java
@@ -2,7 +2,6 @@ package com.gym_system.PS_fitness.security;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
 
